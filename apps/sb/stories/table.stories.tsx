ff8c7674c15dc9323f22cf2e7739f00ffd7e9e87
cieloazul310 <cieloazul310@gmail.com>
2025-06-19 13:40:39 +0900

    chore(sb): update

--- apps/sb/stories/table.stories.tsx
+++ apps/sb/stories/table.stories.tsx
@@ -26,9 +26,9 @@ export default meta;
 type Story = StoryObj<typeof Table.Root>;
 
 export const Playground: Story = {
-  render: ({ ...props }) => {
-    return (
-      <Table.Root {...props}>
+  args: {
+    children: (
+      <>
         <Table.Caption>テーブル</Table.Caption>
         <colgroup>
           <styled.col
@@ -53,32 +53,125 @@ export const Playground: Story = {
           </Table.Row>
         </Table.Head>
         <Table.Body>
+          {Array.from({ length: 4 }).map((_, index) => (
+            <Table.Row key={index}>
+              <Table.Header scope="row">データ</Table.Header>
+              <Table.Cell>データ</Table.Cell>
+              <Table.Cell>データ</Table.Cell>
+              <Table.Cell>データ</Table.Cell>
+              <Table.Cell>データ</Table.Cell>
+              <Table.Cell>データ</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </>
+    ),
+  },
+};
+
+export const TableHeaderWithRowspan: Story = {
+  args: {
+    children: (
+      <>
+        <colgroup>
+          <styled.col
+            borderRightWidth="1px"
+            borderColor="solid-gray.420"
+            bg="solid-gray.50"
+          />
+          <styled.col
+            borderRightWidth="1px"
+            borderColor="black"
+            bg="solid-gray.50"
+          />
+          <col />
+          <col />
+          <col />
+          <col />
+        </colgroup>
+        <Table.Body>
           <Table.Row>
-            <Table.Cell>データ</Table.Cell>
-            <Table.Cell>データ</Table.Cell>
+            <Table.Header rowSpan={2} scope="row">
+              親ラベル
+            </Table.Header>
+            <Table.Header scope="row">子ラベル</Table.Header>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
           </Table.Row>
           <Table.Row>
-            <Table.Cell>データ</Table.Cell>
-            <Table.Cell>データ</Table.Cell>
+            <Table.Header scope="row">子ラベル</Table.Header>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
           </Table.Row>
           <Table.Row>
+            <Table.Header rowSpan={2} scope="row">
+              親ラベル
+            </Table.Header>
+            <Table.Header scope="row">子ラベル</Table.Header>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
+            <Table.Cell>データ</Table.Cell>
+            <Table.Cell>データ</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Header scope="row">子ラベル</Table.Header>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
             <Table.Cell>データ</Table.Cell>
           </Table.Row>
         </Table.Body>
-      </Table.Root>
-    );
+      </>
+    ),
+  },
+};
+
+export const IndentedRows: Story = {
+  args: {
+    children: (
+      <>
+        <styled.col width={8} />
+        <Table.Head>
+          <Table.Row>
+            <Table.Cell colSpan={2} />
+            <Table.Header scope="col">代表者名</Table.Header>
+            <Table.Header scope="col">電話番号</Table.Header>
+            <Table.Header scope="col">住所</Table.Header>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          <Table.Row>
+            <Table.Header colSpan={2} scope="row">
+              東京本社
+            </Table.Header>
+            <Table.Cell>山田太郎</Table.Cell>
+            <Table.Cell>03-1234-5678</Table.Cell>
+            <Table.Cell>東京都新宿区新宿字恐山</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Header scope="row">
+              <styled.span srOnly>東京本社</styled.span>
+            </Table.Header>
+            <Table.Header scope="row">営業部</Table.Header>
+            <Table.Cell>佐藤花子</Table.Cell>
+            <Table.Cell>03-2345-6789</Table.Cell>
+            <Table.Cell>東京都渋谷区4-5-6</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Header scope="row">
+              <styled.span srOnly>東京本社</styled.span>
+            </Table.Header>
+            <Table.Header scope="row">開発部</Table.Header>
+            <Table.Cell>鈴木一郎</Table.Cell>
+            <Table.Cell>03-3456-7890</Table.Cell>
+            <Table.Cell>東京都港区7-8-9</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </>
+    ),
   },
 };
