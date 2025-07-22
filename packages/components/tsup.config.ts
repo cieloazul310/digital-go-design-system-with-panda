a48d11e090806e4818bbc4ef841cc5efdadd14b6
cieloazul310 <cieloazul310@gmail.com>
2025-07-22 22:54:41 +0900

    chore: update

--- packages/components/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src"],
-  format: ["esm", "cjs"],
-  external: ["@cieloazul310/styled-system"],
-  // dts: true,
-  clean: true,
-});
