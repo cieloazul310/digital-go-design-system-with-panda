1d5aab059bdd852a8a8f15abbe54ca0a594c3824
cieloazul310 <cieloazul310@gmail.com>
2025-08-17 18:44:15 +0900

    chore: update

--- apps/nextjs/src/demo/chip-label.tsx
+++ apps/nextjs/src/demo/chip-label.tsx
@@ -1,13 +1,103 @@
-import { Stack } from "styled-system/jsx";
+import {
+  CheckCircle2Icon,
+  CircleXIcon,
+  TriangleAlert,
+  InfoIcon,
+} from "lucide-react";
+import { VStack, HStack } from "styled-system/jsx";
 import { ChipLabel } from "@/components/ui";
 
 export default function App() {
   return (
-    <Stack gap={4}>
-      <ChipLabel>Example Chip</ChipLabel>
-      <ChipLabel variant="outlined">Example Chip Outline</ChipLabel>
-      <ChipLabel variant="solid-fill">Example Chip Solid Fill</ChipLabel>
-      <ChipLabel variant="ghost">Example Chip Ghost</ChipLabel>
-    </Stack>
+    <VStack gap={8}>
+      <HStack gap={4}>
+        <ChipLabel>
+          <CheckCircle2Icon />
+          デフォルト
+        </ChipLabel>
+        <ChipLabel colorPalette="green">
+          <CheckCircle2Icon />
+          成功
+        </ChipLabel>
+        <ChipLabel colorPalette="red">
+          <CircleXIcon />
+          失敗
+        </ChipLabel>
+        <ChipLabel colorPalette="yellow">
+          <TriangleAlert />
+          注意
+        </ChipLabel>
+        <ChipLabel colorPalette="solid-gray">
+          <InfoIcon />
+          情報
+        </ChipLabel>
+      </HStack>
+      <HStack gap={4}>
+        <ChipLabel variant="outlined">
+          <CheckCircle2Icon />
+          デフォルト
+        </ChipLabel>
+        <ChipLabel colorPalette="green" variant="outlined">
+          <CheckCircle2Icon />
+          成功
+        </ChipLabel>
+        <ChipLabel colorPalette="red" variant="outlined">
+          <CircleXIcon />
+          失敗
+        </ChipLabel>
+        <ChipLabel colorPalette="yellow" variant="outlined">
+          <TriangleAlert />
+          注意
+        </ChipLabel>
+        <ChipLabel colorPalette="solid-gray" variant="outlined">
+          <InfoIcon />
+          情報
+        </ChipLabel>
+      </HStack>
+      <HStack gap={4}>
+        <ChipLabel variant="ghost">
+          <CheckCircle2Icon />
+          デフォルト
+        </ChipLabel>
+        <ChipLabel colorPalette="green" variant="ghost">
+          <CheckCircle2Icon />
+          成功
+        </ChipLabel>
+        <ChipLabel colorPalette="red" variant="ghost">
+          <CircleXIcon />
+          失敗
+        </ChipLabel>
+        <ChipLabel colorPalette="yellow" variant="ghost">
+          <TriangleAlert />
+          注意
+        </ChipLabel>
+        <ChipLabel colorPalette="solid-gray" variant="ghost">
+          <InfoIcon />
+          情報
+        </ChipLabel>
+      </HStack>
+      <HStack gap={4}>
+        <ChipLabel variant="solid-fill">
+          <CheckCircle2Icon />
+          デフォルト
+        </ChipLabel>
+        <ChipLabel colorPalette="green" variant="solid-fill">
+          <CheckCircle2Icon />
+          成功
+        </ChipLabel>
+        <ChipLabel colorPalette="red" variant="solid-fill">
+          <CircleXIcon />
+          失敗
+        </ChipLabel>
+        <ChipLabel colorPalette="yellow" variant="solid-fill">
+          <TriangleAlert />
+          注意
+        </ChipLabel>
+        <ChipLabel colorPalette="solid-gray" variant="solid-fill">
+          <InfoIcon />
+          情報
+        </ChipLabel>
+      </HStack>
+    </VStack>
   );
 }
