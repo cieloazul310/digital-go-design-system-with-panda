6a5ba8cb8e515218821cee653ef6bdc67f31a289
cieloazul310 <cieloazul310@gmail.com>
2025-08-26 10:40:21 +0900

    chore: update

--- scripts/update-readme.ts
+++ scripts/update-readme.ts
@@ -5,29 +5,49 @@ import { loadCatalogue } from "./load-catalogue";
 
 const catalog = loadCatalogue();
 
-const tableHeader =
-  "| Name | Recipe | Storybook |\n|------|--------|-----------|";
+function createTable() {
+  const tableHeader =
+    "| 名称 | className | Recipe | Storybook |\n|------|---------|--------|-----------|";
 
-function recipeURL(id: string) {
-  return `https://github.com/cieloazul310/digital-go-design-system-with-panda/blob/main/packages/preset/src/recipes/${id}.ts`;
+  function recipeURL(id: string) {
+    return `https://github.com/cieloazul310/digital-go-design-system-with-panda/blob/main/packages/preset/src/recipes/${id}.ts`;
+  }
+
+  const tableRows = Object.entries(catalog.components)
+    .map(([_, { name, id, storybook }]) => {
+      if (!storybook) {
+        return `| ${name} | ${id} | [Recipe](${recipeURL(id)}) | |`;
+      }
+      return `| ${name} | ${id} | [Recipe](${recipeURL(id)}) | [Storybook](${storybook}) |`;
+    })
+    .join("\n");
+
+  return [tableHeader, tableRows].join("\n");
 }
 
-const tableRows = Object.entries(catalog.components)
-  .map(
-    ([_, { name, id, storybook }]) =>
-      `| ${name} | [Recipe](${recipeURL(id)}) | [Storybook](${storybook}) |`,
-  )
-  .join("\n");
+const newTable = createTable();
 
-const newTable = [tableHeader, tableRows].join("\n");
+function updateReadme(readmePath: string) {
+  const readme = fs.readFileSync(readmePath, "utf8");
+  const isMdx = /.mdx$/.test(readmePath);
+  let updated: string;
 
-const readmePath = "README.md";
-const readme = fs.readFileSync(readmePath, "utf8");
+  if (isMdx) {
+    updated = readme.replace(
+      /\{\/\* @catalog-start \*\/\}([\s\S]*?)\{\/\* @catalog-end \*\/\}/,
+      `\{\/\* @catalog-start \*\/\}\n${newTable}\n\{\/\* @catalog-end \*\/\}`,
+    );
+  } else {
+    updated = readme.replace(
+      /<!-- @catalog-start -->([\s\S]*?)<!-- @catalog-end -->/,
+      `<!-- @catalog-start -->\n${newTable}\n<!-- @catalog-end -->`,
+    );
+  }
 
-const updated = readme.replace(
-  /<!-- @catalog-start -->([\s\S]*?)<!-- @catalog-end -->/,
-  `<!-- @catalog-start -->\n${newTable}\n<!-- @catalog-end -->`,
-);
+  fs.writeFileSync(readmePath, updated);
+  console.log("README.md updated!");
+}
 
-fs.writeFileSync(readmePath, updated);
-console.log("README.md updated!");
+["../README.md", "../apps/nextjs/src/app/(mdx)/page.mdx"]
+  .map((file) => path.resolve(__dirname, file))
+  .forEach(updateReadme);
