610ecf2cf2faa87223f3b7a4b028f7a81c56732d
cieloazul310 <cieloazul310@gmail.com>
2025-12-08 21:29:33 +0900

    chore(preset): リンクのActive時の色をOrange-700からOrange-800に変更 (v2.8.1)

--- packages/preset/src/recipes/link.ts
+++ packages/preset/src/recipes/link.ts
@@ -19,7 +19,7 @@ export default defineRecipe({
       _visited: "magenta.900",
       _hover: "colorPalette.primary.100",
       _focusVisible: "colorPalette.primary.100",
-      _active: "orange.700",
+      _active: "orange.800",
     },
     /**
      * focus-visible:bg-yellow-300
