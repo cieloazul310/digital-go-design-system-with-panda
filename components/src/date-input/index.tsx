44ff7e8181ded2cfc8ce3034bc0b2b69f8578139
cieloazul310 <cieloazul310@gmail.com>
2026-05-21 15:26:03 +0900

    feat(components): add date-input (in progress)

---
+++ components/src/date-input/index.tsx
@@ -0,0 +1,8 @@
+export { useDateInput, useDateInputContext } from "@ark-ui/react/date-input";
+
+export type {
+  UseDateInputProps,
+  UseDateInputReturn,
+} from "@ark-ui/react/date-input";
+
+export * as DateInput from "./snippet";
--- packages/preset/src/recipes/index.ts
+++ packages/preset/src/recipes/index.ts
@@ -7,6 +7,7 @@ import checkbox from "./checkbox";
 import chipLabel from "./chip-label";
 import chipTag from "./chip-tag";
 import datePicker from "./date-picker";
+import dateInput from "./date-input";
 import descriptionList from "./description-list";
 import disclosure from "./disclosure";
 import digitalGoDivider from "./divider";
@@ -51,6 +52,7 @@ const recipes = {
   chipLabel,
   chipTag,
   datePicker,
+  dateInput,
   descriptionList,
   disclosure,
   digitalGoDivider,
